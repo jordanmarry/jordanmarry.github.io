6c8ab62cba8b863c3db9013bc3db143c7d13d79b
Jordan Marry <jmarry@terpmail.umd.edu>
2023-07-10 22:34:53 -0400

    basic implementation

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
