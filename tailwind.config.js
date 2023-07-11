9541d0d9dd0e669bc93c96a7662484217610e41b
Jordan Marry <jmarry@terpmail.umd.edu>
2023-07-10 22:47:19 -0400

    navbar implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.js',
+    './components/**/*.js',
+    './src/**/*.js',
   ],
   theme: {
     extend: {
@@ -11,6 +11,13 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'light-blue': '#0ea0ee',
+        'dark-blue': '#43526b',
+        'darker-blue': '#293241',
+        'white': '#ccd6f6',
+        'grey': '#b4bacc',
       },
     },
   },
